d82beaa20e3c223a9c71dcfcc6a2fc42fd049d0c
yusufhacktiv8 <yusuf.hacktiv8@gmail.com>
2018-10-03 01:13:25 +0700

    Month picker

--- lib/Dashboard.dart
+++ lib/Dashboard.dart
@@ -4,6 +4,8 @@ import 'package:dashboard/components/DashboardItem.dart';
 import 'package:dashboard/components/DashboardItem2.dart';
 import 'package:dashboard/components/DashboardItem3.dart';
 import 'package:dashboard/components/DashboardItem4.dart';
+import 'package:intl/intl.dart';
+import 'package:month_picker_strip/month_picker_strip.dart';
 
 class Dashboard extends StatelessWidget {
   @override
@@ -14,6 +16,19 @@ class Dashboard extends StatelessWidget {
           centerTitle: true,
         ),
         body: new ListView(children: <Widget>[
+          new MonthStrip(
+            format: 'MMM yyyy',
+            from: new DateTime(2016, 4),
+            to: new DateTime(2018, 5),
+            initialMonth: new DateTime(2018, 1),
+            height: 58.0,
+            viewportFraction: 0.45,
+            onMonthChanged: (v) {
+
+            },
+            normalTextStyle: TextStyle(fontSize: 16.0, color: Colors.blueGrey),
+            selectedTextStyle: TextStyle(fontSize: 20.0, color: Colors.blueAccent),
+          ),
           new MainCharts(),
           DashboardItem(),
           Divider(height: 1.0, color: Colors.grey,),
