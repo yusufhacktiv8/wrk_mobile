2c9c0a9eb9c7b684a0e9411260dc982e06bc9b34
yusufhacktiv8 <yusuf.hacktiv8@gmail.com>
2018-10-12 01:40:45 +0700

    Project page details

--- lib/pages/ProjectItem.dart
+++ lib/pages/ProjectItem.dart
@@ -58,6 +58,121 @@ class ProjectItem extends StatelessWidget {
                   ],
                 ),
               ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text("BAD",
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.bold)),
+                    Text(project.bad.toString(),
+                        style: TextStyle(fontSize: 17.0))
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Text("OK",
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.bold)),
+                    Text(project.ok.toString(),
+                        style: TextStyle(fontSize: 17.0))
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text("Ra Progress",
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.bold)),
+                    Text(project.raProgress.toString(),
+                        style: TextStyle(fontSize: 17.0))
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Text("Ri Progress",
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.bold)),
+                    Text(project.riProgress.toString(),
+                        style: TextStyle(fontSize: 17.0))
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text("Piutang Usaha",
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.bold)),
+                    Text(project.piutangUsaha.toString(),
+                        style: TextStyle(fontSize: 17.0))
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Text("Piutang Retensi",
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.bold)),
+                    Text(project.piutangRetensi.toString(),
+                        style: TextStyle(fontSize: 17.0))
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text("Tagihan Bruto",
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.bold)),
+                    Text(project.tagihanBruto.toString(),
+                        style: TextStyle(fontSize: 17.0))
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Text("Persediaan",
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.bold)),
+                    Text(project.persediaan.toString(),
+                        style: TextStyle(fontSize: 17.0))
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text("Cashflow",
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.bold)),
+                    Text(project.cashflow.toString(),
+                        style: TextStyle(fontSize: 17.0))
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Text("Laba Kotor",
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.bold)),
+                    Text(project.labaKotor.toString(),
+                        style: TextStyle(fontSize: 17.0))
+                  ],
+                ),
+              ],
             )
           ],
         ),
--- lib/states/Project.dart
+++ lib/states/Project.dart
@@ -4,13 +4,33 @@ class Project {
   final String name;
   final double ra;
   final double ri;
+  final double raProgress;
+  final double riProgress;
   final double pdp;
+  final double bad;
+  final double ok;
+  final double piutangUsaha;
+  final double piutangRetensi;
+  final double tagihanBruto;
+  final double persediaan;
+  final double cashflow;
+  final double labaKotor;
 
   Project({
     this.name,
     this.ra,
     this.ri,
     this.pdp,
+    this.bad,
+    this.ok,
+    this.raProgress,
+    this.riProgress,
+    this.piutangUsaha,
+    this.piutangRetensi,
+    this.tagihanBruto,
+    this.persediaan,
+    this.cashflow,
+    this.labaKotor,
   });
 
   static List<Project> fromJsonArray(String jsonArrayString){
@@ -21,7 +41,17 @@ class Project {
         name: data[i]["Project"]["name"],
           ra: data[i]["ra"] != null ? (data[i]["ra"] as num).toDouble() : 0.0,
           ri: data[i]["ri"] != null ? (data[i]["ri"] as num).toDouble() : 0.0,
+          raProgress: data[i]["raProgress"] != null ? (data[i]["raProgress"] as num).toDouble() : 0.0,
+          riProgress: data[i]["riProgress"] != null ? (data[i]["riProgress"] as num).toDouble() : 0.0,
           pdp: data[i]["pdp"] != null ? (data[i]["pdp"] as num).toDouble() : 0.0,
+          bad: data[i]["bad"] != null ? (data[i]["bad"] as num).toDouble() : 0.0,
+          ok: data[i]["ok"] != null ? (data[i]["ok"] as num).toDouble() : 0.0,
+          piutangUsaha: data[i]["piutangUsaha"] != null ? (data[i]["piutangUsaha"] as num).toDouble() : 0.0,
+          piutangRetensi: data[i]["piutangRetensi"] != null ? (data[i]["piutangRetensi"] as num).toDouble() : 0.0,
+          tagihanBruto: data[i]["tagihanBruto"] != null ? (data[i]["tagihanBruto"] as num).toDouble() : 0.0,
+          persediaan: data[i]["persediaan"] != null ? (data[i]["persediaan"] as num).toDouble() : 0.0,
+          cashflow: data[i]["cashflow"] != null ? (data[i]["cashflow"] as num).toDouble() : 0.0,
+          labaKotor: data[i]["labaKotor"] != null ? (data[i]["labaKotor"] as num).toDouble() : 0.0,
       ));
     }
     return result;
