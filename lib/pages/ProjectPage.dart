a366227f761232d3aaa6d425aabbd9cf5c2409a2
yusufhacktiv8 <yusuf.hacktiv8@gmail.com>
2018-10-11 23:22:12 +0700

    Project page fix

--- lib/pages/ProjectPage.dart
+++ lib/pages/ProjectPage.dart
@@ -5,6 +5,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:dashboard/Constant.dart';
 import 'package:dashboard/states/Project.dart';
+import 'package:month_picker_strip/month_picker_strip.dart';
 
 class ProjectPage extends StatefulWidget {
   final int year, month, projectType;
@@ -16,10 +17,12 @@ class ProjectPage extends StatefulWidget {
 
 class _ProjectPageState extends State<ProjectPage> {
   List<Project> projects = [];
+  DateTime _selectedMonth;
 
   @override
   void initState() {
     super.initState();
+    _selectedMonth = new DateTime(widget.year, widget.month);
     _getProjects(widget.year, widget.month, widget.projectType);
   }
 
@@ -29,19 +32,41 @@ class _ProjectPageState extends State<ProjectPage> {
       title: new Text('Dashboard'),
       centerTitle: true,
     ),
-    body:ListView.builder(
-      itemBuilder: (BuildContext context, int index) =>
-        Column(
-          children: <Widget>[
-            ListTile(
-              title: Text(projects[index].name, style: TextStyle(fontSize: 17.0)),
-                trailing: const Icon(Icons.arrow_forward_ios, size: 16.0,)
-            ),
-            Divider(height: 1.0, color: Colors.grey,),
-          ],
+    body: Column(
+      children: <Widget>[
+        new MonthStrip(
+          format: 'MMM yyyy',
+          from: new DateTime(2016, 4),
+          to: new DateTime(2018, 5),
+          initialMonth: _selectedMonth,
+          height: 58.0,
+          viewportFraction: 0.45,
+          onMonthChanged: (v) {
+            setState(() {
+              _selectedMonth = v;
+            });
+          },
+          normalTextStyle: TextStyle(fontSize: 16.0, color: Colors.blueGrey),
+          selectedTextStyle: TextStyle(fontSize: 20.0, color: Colors.blueAccent),
+        ),
+        new Expanded(
+          child: ListView.builder(
+            itemBuilder: (BuildContext context, int index) =>
+                Column(
+                  children: <Widget>[
+                    ListTile(
+                        title: Text(projects[index].name, style: TextStyle(fontSize: 17.0)),
+                        trailing: const Icon(Icons.arrow_forward_ios, size: 16.0,)
+                    ),
+                    Divider(height: 1.0, color: Colors.grey,),
+                  ],
+                )
+            ,
+            itemCount: projects.length,
+          )
         )
-          ,
-      itemCount: projects.length,
+
+      ],
     ));
   }
 
