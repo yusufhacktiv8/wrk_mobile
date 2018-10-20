588e58ee3110574da7620a51b4fc2d7ba4b04e02
yusufhacktiv8 <yusuf.hacktiv8@gmail.com>
2018-10-20 17:20:43 +0700

    Fix dashboard

--- lib/pages/dashboard/Dashboard.dart
+++ lib/pages/dashboard/Dashboard.dart
@@ -75,182 +75,189 @@ class _DashboardState extends State<Dashboard> {
             )
           ],
         ),
-        body: StaggeredGridView.count(
-          crossAxisCount: 2,
-          crossAxisSpacing: 12.0,
-          mainAxisSpacing: 12.0,
-          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+        body: Column(
           children: <Widget>[
-            _buildTile(
-              Padding(
-                  padding: const EdgeInsets.only(left: 12.0, right: 12.0, top: 15.0, bottom: 0.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          ChartLabel(),
-                          DropdownButton(
-                              isDense: true,
-                              value: actualDropdown,
-                              onChanged: (String value) {
-                                setState(() {
-                                  actualDropdown = value;
-                                  actualChart = chartDropdownItems
-                                      .indexOf(value); // Refresh the chart
-                                });
-                                eventBus.fire(LineChartChangedEvent(null));
-                              },
-                              items: chartDropdownItems.map((String title) {
-                                return DropdownMenuItem(
-                                  value: title,
-                                  child: Text(title,
-                                      style: TextStyle(
-                                          color: Colors.blue,
-                                          fontWeight: FontWeight.w400,
-                                          fontSize: 16.0)),
-                                );
-                              }).toList())
-                        ],
-                      ),
-                      Padding(padding: EdgeInsets.only(bottom: 1.0)),
+            Text("Hello"),
+            Expanded(
+              child: StaggeredGridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 12.0,
+                mainAxisSpacing: 12.0,
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                children: <Widget>[
+                  _buildTile(
+                    Padding(
+                        padding: const EdgeInsets.only(left: 12.0, right: 12.0, top: 15.0, bottom: 0.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                ChartLabel(),
+                                DropdownButton(
+                                    isDense: true,
+                                    value: actualDropdown,
+                                    onChanged: (String value) {
+                                      setState(() {
+                                        actualDropdown = value;
+                                        actualChart = chartDropdownItems
+                                            .indexOf(value); // Refresh the chart
+                                      });
+                                      eventBus.fire(LineChartChangedEvent(null));
+                                    },
+                                    items: chartDropdownItems.map((String title) {
+                                      return DropdownMenuItem(
+                                        value: title,
+                                        child: Text(title,
+                                            style: TextStyle(
+                                                color: Colors.blue,
+                                                fontWeight: FontWeight.w400,
+                                                fontSize: 16.0)),
+                                      );
+                                    }).toList())
+                              ],
+                            ),
+                            Padding(padding: EdgeInsets.only(bottom: 1.0)),
 //                      Sparkline(
 //                        data: charts[actualChart],
 //                        lineWidth: 5.0,
 //                        lineColor: Colors.greenAccent,
 //                      )
-                      OmzetChart(year: 2018, chartType: chartType,),
-                    ],
-                  )),
-            ),
-            _buildTile(
-              Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text('Total Views',
-                              style: TextStyle(color: Colors.blueAccent)),
-                          Text('265K',
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: 34.0))
-                        ],
-                      ),
-                      Material(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.circular(24.0),
-                          child: Center(
-                              child: Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Icon(Icons.timeline,
-                                color: Colors.white, size: 30.0),
-                          )))
-                    ]),
-              ),
-            ),
-            _buildTile(
-              Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Material(
-                          color: Colors.teal,
-                          shape: CircleBorder(),
-                          child: Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Icon(Icons.settings_applications,
-                                color: Colors.white, size: 30.0),
-                          )),
-                      Padding(padding: EdgeInsets.only(bottom: 16.0)),
-                      Text('General',
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.w700,
-                              fontSize: 24.0)),
-                      Text('Images, Videos',
-                          style: TextStyle(color: Colors.black45)),
-                    ]),
-              ),
-            ),
-            _buildTile(
-              Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Material(
-                          color: Colors.amber,
-                          shape: CircleBorder(),
-                          child: Padding(
-                            padding: EdgeInsets.all(16.0),
-                            child: Icon(Icons.notifications,
-                                color: Colors.white, size: 30.0),
-                          )),
-                      Padding(padding: EdgeInsets.only(bottom: 16.0)),
-                      Text('Alerts',
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.w700,
-                              fontSize: 24.0)),
-                      Text('All ', style: TextStyle(color: Colors.black45)),
-                    ]),
-              ),
-            ),
-            _buildTile(
-              Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text('Shop Items',
-                              style: TextStyle(color: Colors.redAccent)),
-                          Text('173',
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: 34.0))
-                        ],
-                      ),
-                      Material(
-                          color: Colors.red,
-                          borderRadius: BorderRadius.circular(24.0),
-                          child: Center(
-                              child: Padding(
-                            padding: EdgeInsets.all(16.0),
-                            child: Icon(Icons.store,
-                                color: Colors.white, size: 30.0),
-                          )))
-                    ]),
-              ),
+                            OmzetChart(year: 2018, chartType: chartType,),
+                          ],
+                        )),
+                  ),
+                  _buildTile(
+                    Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text('Total Views',
+                                    style: TextStyle(color: Colors.blueAccent)),
+                                Text('265K',
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.w700,
+                                        fontSize: 34.0))
+                              ],
+                            ),
+                            Material(
+                                color: Colors.blue,
+                                borderRadius: BorderRadius.circular(24.0),
+                                child: Center(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(16.0),
+                                      child: Icon(Icons.timeline,
+                                          color: Colors.white, size: 30.0),
+                                    )))
+                          ]),
+                    ),
+                  ),
+                  _buildTile(
+                    Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Material(
+                                color: Colors.teal,
+                                shape: CircleBorder(),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(16.0),
+                                  child: Icon(Icons.settings_applications,
+                                      color: Colors.white, size: 30.0),
+                                )),
+                            Padding(padding: EdgeInsets.only(bottom: 16.0)),
+                            Text('General',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 24.0)),
+                            Text('Images, Videos',
+                                style: TextStyle(color: Colors.black45)),
+                          ]),
+                    ),
+                  ),
+                  _buildTile(
+                    Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Material(
+                                color: Colors.amber,
+                                shape: CircleBorder(),
+                                child: Padding(
+                                  padding: EdgeInsets.all(16.0),
+                                  child: Icon(Icons.notifications,
+                                      color: Colors.white, size: 30.0),
+                                )),
+                            Padding(padding: EdgeInsets.only(bottom: 16.0)),
+                            Text('Alerts',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 24.0)),
+                            Text('All ', style: TextStyle(color: Colors.black45)),
+                          ]),
+                    ),
+                  ),
+                  _buildTile(
+                    Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text('Shop Items',
+                                    style: TextStyle(color: Colors.redAccent)),
+                                Text('173',
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.w700,
+                                        fontSize: 34.0))
+                              ],
+                            ),
+                            Material(
+                                color: Colors.red,
+                                borderRadius: BorderRadius.circular(24.0),
+                                child: Center(
+                                    child: Padding(
+                                      padding: EdgeInsets.all(16.0),
+                                      child: Icon(Icons.store,
+                                          color: Colors.white, size: 30.0),
+                                    )))
+                          ]),
+                    ),
 //              onTap: () => Navigator
 //                  .of(context)
 //                  .push(MaterialPageRoute(builder: (_) => ShopItemsPage())),
+                  )
+                ],
+                staggeredTiles: [
+                  StaggeredTile.extent(2, 220.0),
+                  StaggeredTile.extent(2, 110.0),
+                  StaggeredTile.extent(1, 180.0),
+                  StaggeredTile.extent(1, 180.0),
+                  StaggeredTile.extent(2, 110.0),
+                ],
+              ),
             )
-          ],
-          staggeredTiles: [
-            StaggeredTile.extent(2, 220.0),
-            StaggeredTile.extent(2, 110.0),
-            StaggeredTile.extent(1, 180.0),
-            StaggeredTile.extent(1, 180.0),
-            StaggeredTile.extent(2, 110.0),
           ],
         ));
   }
