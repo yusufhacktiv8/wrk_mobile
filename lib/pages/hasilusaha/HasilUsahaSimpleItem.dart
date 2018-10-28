288f39c098f3f40acc55424f9752e767dadc88d3
yusufhacktiv8 <yusuf.hacktiv8@gmail.com>
2018-10-28 22:58:59 +0700

    Hasil usaha completion

--- lib/pages/hasilusaha/HasilUsahaSimpleItem.dart
+++ lib/pages/hasilusaha/HasilUsahaSimpleItem.dart
@@ -22,45 +22,64 @@ class HasilUsahaSimpleItem extends StatelessWidget {
     String riText = ri != null ? fn.format((ri as num).toDouble()) : "-";
     String prognosaText = prognosa != null ? fn.format((prognosa as num).toDouble()) : "-";
 
-    return Column(
-      children: <Widget>[
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Text("RKAP ", style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
-                Text(rkapText, style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Text("Ra s/d Saat Ini ", style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
-                Text(raText, style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
-              ],
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      child: ListView(children: <Widget>[
+        Card(
+            child: Container(
+                padding: EdgeInsets.all(8.0),
+                height: 120.0,
+                child: ListTile(
+                  subtitle: Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text("RKAP ", style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
+                              Text(rkapText, style: TextStyle(fontSize: 18.0)),
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: <Widget>[
+                              Text("Ra s/d Saat Ini ", style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
+                              Text(raText, style: TextStyle(fontSize: 18.0)),
+                            ],
+                          )
+                        ],
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(5.0),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text("Ri s/d Saat Ini ", style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
+                              Text(riText, style: TextStyle(fontSize: 18.0)),
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: <Widget>[
+                              Text("Prognosa ", style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
+                              Text(prognosaText, style: TextStyle(fontSize: 18.0)),
+                            ],
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                )
             )
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Text("Ri s/d Saat Ini ", style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
-                Text(riText, style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Text("Prognosa ", style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
-                Text(prognosaText, style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
-              ],
-            )
-          ],
-        ),
-      ],
+          )
+      ],),
     );
   }
-
 }
 
