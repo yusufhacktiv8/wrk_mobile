64988756bee1bee996286b187e955ee21cbdf132
yusufhacktiv8 <yusuf.hacktiv8@gmail.com>
2018-11-04 21:54:25 +0700

    Smwg page completion

--- lib/pages/smwg/SmwgPageItem.dart
+++ lib/pages/smwg/SmwgPageItem.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:dashboard/models/SmwgItem.dart';
+import 'package:intl/intl.dart';
 
 class SmwgPageItem extends StatelessWidget {
+  final fn = new NumberFormat("#,###.00");
   final SmwgItem smwgItem;
 
   SmwgPageItem({Key key, this.smwgItem}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-        title: Text(smwgItem.name,
-            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
-        subtitle: Row(
+    String bobotText = smwgItem.bobot != null ? fn.format((smwgItem.bobot as num).toDouble()) : "-";
+    String nilaiText = smwgItem.nilai != null ? fn.format((smwgItem.nilai as num).toDouble()) : "-";
+    return Container(
+      padding: EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-            Text(smwgItem.bobot.toString()),
-            Text(" - "),
-            Text(smwgItem.nilai.toString()),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(10.0),
+                child: Text(smwgItem.name, style: TextStyle(fontSize: 18.0),),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              width: 75.0,
+              child: Text(bobotText, style: TextStyle(fontSize: 18.0), textAlign: TextAlign.end,),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              width: 75.0,
+              child: Text(nilaiText, style: TextStyle(fontSize: 18.0), textAlign: TextAlign.end,),
+            ),
           ],
         ));
   }
